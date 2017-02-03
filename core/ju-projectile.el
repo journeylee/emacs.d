8136048cb33c24849b341c41d0813a62d0ccea16
李雎 <journeylee@lijudeMac-mini.lan>
2017-02-04 00:27:58 +0800

    make projectile active by default

--- core/ju-projectile.el
+++ core/ju-projectile.el
@@ -3,6 +3,7 @@
   :config (progn
             (projectile-global-mode t)
 	    (setq projectile-enable-caching t)
-	    (setq projectile-file-exists-remote-cache-expire (* 10 60))))
+	    (setq projectile-file-exists-remote-cache-expire (* 10 60)))
+  :diminish projectile-mode)
 
 (provide 'ju-projectile)
